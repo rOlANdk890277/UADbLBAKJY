e9c13c50d8f0182f6f4935064673136c80892a20
rOlANdk890277 <wishes-bushy-0u@icloud.com>
2025-07-31 23:55:09 +0100

    Create 单元测试框架

---
+++ testable_component_0801_0655_bik.vue
@@ -0,0 +1,40 @@
+// 代码生成时间: 2025-08-01 06:55:08
+import { ref, onMounted } from 'vue';
+
+// 响应式数据
+const message = ref('Hello, this is a test component!');
+
+// 生命周期钩子
+onMounted(() => {
+  console.log('Component is mounted');
+});
+
+// 计算属性和方法
+function reverseMessage() {
+  message.value = message.value.split('').reverse().join('');
+}
+</script>
+
+<style scoped>
+h2 {
+  color: blue;
+}
+button {
+  margin-top: 20px;
+}
+</style>
+
+<script>
+// 单元测试框架
+import { describe, it, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
+import TestableComponent from './TestableComponent.vue';
+
+describe('TestableComponent', () => {
+  it('should reverse the message on button click', async () => {
+    const wrapper = mount(TestableComponent);
+    await wrapper.find('button').trigger('click');
+    expect(wrapper.text()).toContain(wrapper.text().split('').reverse().join(''));
+  });
+});
+</script>
