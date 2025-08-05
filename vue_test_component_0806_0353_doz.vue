f1c3d3a2fea6e00084acf063688d4d623376dadd
rOlANdk890277 <wishes-bushy-0u@icloud.com>
2025-08-05 20:53:44 +0100

    Configure 集成测试工具

---
+++ vue_test_component_0806_0353_doz.vue
@@ -0,0 +1,24 @@
+// 代码生成时间: 2025-08-06 03:53:42
+// 引入Vue 3的createComponent函数
+import { defineComponent, ref, onMounted } from 'vue';
+
+// 定义组件
+export default defineComponent({
+  name: 'VueTestComponent',
+  // 使用响应式数据
+  setup() {
+    // 定义响应式数据
+    const message = ref('Hello Vue 3!');
+    
+    // 生命周期钩子
+    onMounted(() => {
+      console.log('Component is mounted');
+    });
+    
+    // 返回响应式数据供模板使用
+    return {
+      message
+    };
+  }
+});
+</script>
