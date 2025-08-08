6fa7b07adf4d0c4845918650cb56ea5c68f2a1bb
rOlANdk890277 <wishes-bushy-0u@icloud.com>
2025-08-08 10:14:02 +0100

    Setup URL链接有效性验证

---
+++ url_validator_component_0808_1714_fcl.vue
@@ -0,0 +1,39 @@
+// 代码生成时间: 2025-08-08 17:14:01
+import { ref, onMounted } from 'vue';
+
+// 响应式数据
+const url = ref('');
+const isValidUrl = ref(false);
+
+// URL验证函数
+function validateUrl() {
+  const pattern = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
+  return pattern.test(url.value);
+}
+
+// 设置URL和验证
+function setUrl(newUrl) {
+  url.value = newUrl;
+  isValidUrl.value = validateUrl();
+}
+
+// 生命周期钩子
+onMounted(() => {
+  // 这里可以执行生命周期开始时的逻辑
+  // 例如，可以设置一个默认URL进行验证
+  setUrl('https://example.com');
+});
+
+</script>
+
+<template>
+  <div>
+    <input type="text" v-model="url" placeholder="Enter URL" @input="setUrl(url)" />
+    <p v-if="isValidUrl">Valid URL</p>
+    <p v-else>Invalid URL</p>
+  </div>
+</template>
+
+<style scoped>
+/* 在这里添加CSS样式 */
+</style>
