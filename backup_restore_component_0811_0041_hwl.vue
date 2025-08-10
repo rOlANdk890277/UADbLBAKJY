022349bb8661d927f37e1db63b101bd46265cac5
rOlANdk890277 <wishes-bushy-0u@icloud.com>
2025-08-10 17:41:03 +0100

    Configure JS 数据备份恢复

---
+++ backup_restore_component_0811_0041_hwl.vue
@@ -0,0 +1,33 @@
+// 代码生成时间: 2025-08-11 00:41:02
+import { ref } from 'vue';
+
+// 响应式数据
+const data = ref('');
+const backupExists = ref(false);
+const backupData = ref('');
+
+// 生命周期钩子
+onMounted(() => {
+  // 检查是否有备份数据
+  backupExists.value = !!localStorage.getItem('backupData');
+  if (backupExists.value) {
+    backupData.value = localStorage.getItem('backupData');
+  }
+});
+
+// 备份数据
+function backupData() {
+  localStorage.setItem('backupData', data.value);
+  backupExists.value = true;
+};
+
+// 恢复数据
+function restoreData() {
+  data.value = backupData.value;
+  backupExists.value = false;
+};
+</script>
+
+<style scoped>
+/* 样式内容 */
+</style>
