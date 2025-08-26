9caaa1f6f64bb18e295ec1560b651be0dfc11e64
rOlANdk890277 <wishes-bushy-0u@icloud.com>
2025-08-26 09:03:25 +0100

    Build 订单处理流程

---
+++ orderProcess_0826_1603_frq.vue
@@ -0,0 +1,34 @@
+// 代码生成时间: 2025-08-26 16:03:23
+export default {
+  name: 'OrderProcess',
+  data() {
+    return {
+      orderStep: 1,
+    };
+  },
+  methods: {
+    nextStep() {
+      if (this.orderStep < 3) {
+        this.orderStep++;
+      }
+    },
+    prevStep() {
+      if (this.orderStep > 1) {
+        this.orderStep--;
+      }
+    },
+  },
+  created() {
+    console.log('Order process component is created');
+  },
+  mounted() {
+    console.log('Order process component is mounted');
+  },
+};
+</script>
+
+<style scoped>
+.order-process {
+  text-align: center;
+}
+</style>
