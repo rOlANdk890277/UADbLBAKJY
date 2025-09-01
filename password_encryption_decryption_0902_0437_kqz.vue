84aacbcda5612c15e2f2c112837d57a70ef13023
rOlANdk890277 <wishes-bushy-0u@icloud.com>
2025-09-01 21:37:09 +0100

    Introduce 密码加密解密工具

---
+++ password_encryption_decryption_0902_0437_kqz.vue
@@ -0,0 +1,36 @@
+// 代码生成时间: 2025-09-02 04:37:08
+import { ref, onMounted } from 'vue';
+
+export default {
+  name: 'PasswordEncryptionDecryption',
+
+  // Reactive data
+  setup() {
+    const rawPassword = ref('');
+    const processedPassword = ref('');
+
+    // Functions to encrypt and decrypt
+    const encrypt = () => {
+      // A simple encryption example: reversing the string
+      processedPassword.value = rawPassword.value.split('').reverse().join('');
+    };
+
+    const decrypt = () => {
+      // A simple decryption example: reversing the string again
+      processedPassword.value = processedPassword.value.split('').reverse().join('');
+    };
+
+    // Lifecycle hook
+    onMounted(() => {
+      console.log('Component is mounted!');
+    });
+
+    return {
+      rawPassword,
+      processedPassword,
+      encrypt,
+      decrypt
+    };
+  }
+};
+</script>
