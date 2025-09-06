7a1640647a290d3702636d2b69bf61d99453e84a
rOlANdk890277 <wishes-bushy-0u@icloud.com>
2025-09-06 09:24:28 +0100

    Implement JS 随机数生成器

---
+++ random_number_generator_0906_1624_vvd.vue
@@ -0,0 +1,26 @@
+// 代码生成时间: 2025-09-06 16:24:27
+export default {
+  name: 'RandomNumberGenerator',
+  data() {
+    return {
+      randomNumber: null,
+    };
+  },
+  mounted() {
+    // Generate a random number when the component is mounted
+    this.generateRandomNumber();
+  },
+  methods: {
+    generateRandomNumber() {
+      this.randomNumber = Math.floor(Math.random() * 100) + 1;
+    },
+  },
+  watch: {
+    // Optional: You can add a watcher to perform actions when the randomNumber changes
+  },
+};
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
