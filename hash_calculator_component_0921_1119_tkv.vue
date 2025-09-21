94fb421af8f38b75939f0700463dba5620bcc7a3
rOlANdk890277 <wishes-bushy-0u@icloud.com>
2025-09-21 04:19:24 +0100

    Add 哈希值计算工具 using VUE

---
+++ hash_calculator_component_0921_1119_tkv.vue
@@ -0,0 +1,40 @@
+// 代码生成时间: 2025-09-21 11:19:22
+import { ref, onMounted, onUnmounted } from 'vue';
+import { sha256 } from 'crypto-hash';
+
+export default {
+  name: 'HashCalculator',
+  components: {},
+  props: {},
+  setup() {
+    const inputText = ref('');
+    const hashValue = ref('');
+
+    function calculateHash() {
+      if (inputText.value) {
+        hashValue.value = sha256(inputText.value);
+      } else {
+        hashValue.value = '';
+      }
+    }
+
+    onMounted(() => {
+      calculateHash();
+    });
+
+    onUnmounted(() => {
+      // Perform cleanup, if necessary.
+    });
+
+    return {
+      inputText,
+      hashValue,
+      calculateHash
+    };
+  },
+};
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
