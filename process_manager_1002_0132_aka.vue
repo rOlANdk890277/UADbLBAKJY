37a7643b80d5c470670bf8aa1e2a4c55a9e45845
rOlANdk890277 <wishes-bushy-0u@icloud.com>
2025-10-01 18:32:29 +0100

    Update JS 进程管理器

---
+++ process_manager_1002_0132_aka.vue
@@ -0,0 +1,44 @@
+// 代码生成时间: 2025-10-02 01:32:28
+import { ref, onMounted, reactive } from 'vue';
+
+export default {
+  name: 'ProcessManager',
+  props: {},
+  setup() {
+    // 响应式数据
+    const processes = reactive([
+      { id: 1, name: '进程A' },
+      { id: 2, name: '进程B' },
+      // 假设还有其他进程
+    ]);
+
+    // 生命周期钩子
+    onMounted(() => {
+      console.log('进程管理器组件已挂载');
+    });
+
+    // 方法：终止进程
+    const terminateProcess = (id) => {
+      processes.forEach((process, index) => {
+        if (process.id === id) {
+          processes.splice(index, 1);
+        }
+      });
+    };
+
+    return {
+      processes,
+      terminateProcess
+    };
+  }
+};
+</script>
+
+<style scoped>
+.process-manager {
+  /* 样式代码 */
+}
+.process-item {
+  /* 样式代码 */
+}
+</style>
