02e0737ee102ab0f7c2a9c76f7155b0fe624443c
rOlANdk890277 <wishes-bushy-0u@icloud.com>
2025-10-09 13:26:09 +0100

    Create JS 工作流引擎

---
+++ workflow_engine_1009_2026_vto.vue
@@ -0,0 +1,49 @@
+// 代码生成时间: 2025-10-09 20:26:08
+// 工作流引擎组件
+export default {
+    name: 'WorkflowEngine',
+    data() {
+        // 响应式数据
+        return {
+            tasks: []
+        };
+    },
+    created() {
+        // 组件创建时执行的代码
+        this.fetchTasks();
+    },
+    methods: {
+        // 获取任务数据的方法
+        fetchTasks() {
+            // 假设有一个API可以获取任务数据
+            // 这里使用静态数据模拟
+            this.tasks = [
+                { id: 1, name: 'Task 1' },
+                { id: 2, name: 'Task 2' },
+                { id: 3, name: 'Task 3' }
+            ];
+        }
+    },
+    watch: {
+        // 观察tasks变量的变化
+        tasks(newValue, oldValue) {
+            console.log('Tasks updated:', newValue);
+        }
+    },
+    computed: {
+        // 计算属性，根据tasks数据计算完成任务总数
+        completedTasks() {
+            return this.tasks.filter(task => task.completed).length;
+        }
+    }
+};
+</script>
+
+<style scoped>
+.workflow-engine {
+    /* 工作流引擎组件的样式 */
+}
+.task {
+    /* 任务项的样式 */
+}
+</style>
